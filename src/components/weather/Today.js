e9382b4be1cad401b602095b1c9e055c91fbcfbc
Ji Park <jiyongprk@gmail.com>
2022-05-14 00:43:12 -0400

    fix NaN showing

--- src/components/weather/Today.js
+++ src/components/weather/Today.js
@@ -22,7 +22,7 @@ export default function Today(props) {
             <h1><u>{!props.weatherInfo[0].place ? "" :props.weatherInfo[0].place} {props.weatherInfo[0].country}</u></h1>
             <div className="section-1">
                 <div>
-                    <p>Feels Like: {!toggle? !props.weatherInfo[0].feels? "" :props.weatherInfo[0].feels - 273 + "°C": (props.weatherInfo[0].feels - 273) * (9/5) + 32 + "°F"}</p>
+                    <p>Feels Like: {!toggle? !props.weatherInfo[0].feels? "" :props.weatherInfo[0].feels - 273 + "°C": !props.weatherInfo[0].feels? "": (props.weatherInfo[0].feels - 273) * (9/5) + 32 + "°F"}</p>
                     <img src="https://cdn-icons-png.flaticon.com/512/648/648198.png"/>
                 </div>
                 <div>
@@ -36,15 +36,15 @@ export default function Today(props) {
             </div>
             <div className="section-2">
                 <div>
-                    <p>Temp: {!toggle? !props.weatherInfo[0].temp? "": props.weatherInfo[0].temp - 273 + "°C":(props.weatherInfo[0].temp - 273) * (9/5) + 32 + "°F"}</p>
+                    <p>Temp: {!toggle? !props.weatherInfo[0].temp? "": props.weatherInfo[0].temp - 273 + "°C":!props.weatherInfo[0].temp?"":(props.weatherInfo[0].temp - 273) * (9/5) + 32 + "°F"}</p>
                     <img src="https://cdn-icons-png.flaticon.com/512/3815/3815449.png"/>
                 </div>
                 <div>
-                    <p>Temp Max: {!toggle?!props.weatherInfo[0].tempMax? "": props.weatherInfo[0].tempMax - 273 + "°C":(props.weatherInfo[0].tempMax - 273) * (9/5) + 32 + "°F"}</p>
+                    <p>Temp Max: {!toggle?!props.weatherInfo[0].tempMax? "": props.weatherInfo[0].tempMax - 273 + "°C":!props.weatherInfo[0].tempMax? "" : (props.weatherInfo[0].tempMax - 273) * (9/5) + 32 + "°F"}</p>
                     <img src="https://cdn-icons.flaticon.com/png/512/5826/premium/5826412.png?token=exp=1652492255~hmac=eccb228ceac6a7e787b54a40ce0f5c1f"/>
                 </div>
                 <div>
-                    <p>Temp Min: {!toggle?!props.weatherInfo[0].tempMin? "" : props.weatherInfo[0].tempMin - 273 + "°C":(props.weatherInfo[0].tempMin - 273) * (9/5) + 32 + "°F"}</p>
+                    <p>Temp Min: {!toggle?!props.weatherInfo[0].tempMin? "" : props.weatherInfo[0].tempMin - 273 + "°C":!props.weatherInfo[0].tempMin? "": (props.weatherInfo[0].tempMin - 273) * (9/5) + 32 + "°F"}</p>
                     <img src="https://cdn-icons.flaticon.com/png/512/5826/premium/5826433.png?token=exp=1652492255~hmac=16bac6c18b827cce220ebc17b103823e"/>
                 </div>
             </div>
